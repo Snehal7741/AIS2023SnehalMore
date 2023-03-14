1a3115ce7a554bf4ef5677eccf20894f7b698615
Snehal7741 <111333664+Snehal7741@users.noreply.github.com>
2023-03-14 10:56:41 +0530

    Add files via upload

---
+++ validate.js
@@ -0,0 +1,15 @@
+
+    function validation()
+{
+var mobileno=document.getElementById("mobileno").value 
+ if( mobileno.length==10)
+ {
+    alert('valid number');
+ }
+
+ else{
+    alert('invalid  Mobile number');
+ }
+}
+
+
