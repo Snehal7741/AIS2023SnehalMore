64107f27ec581bca9ec7b04e7c479bbc4350a49f
Snehal7741 <111333664+Snehal7741@users.noreply.github.com>
2023-03-19 23:53:16 -0700

    Add files via upload

--- validate.js
+++ validate.js
@@ -1,15 +1,27 @@
 
     function validation()
 {
+   var email=document.getElementById("email").value
+   var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 var mobileno=document.getElementById("mobileno").value 
- if( mobileno.length==10)
+ if( mobileno.length!=10)
  {
-    alert('valid number');
+    alert(' please enter your valid number');
  }
 
- else{
-    alert('invalid  Mobile number');
- }
+ else if(email=='')
+		{
+			alert('Please enter your user email id');
+		}
+		else if (!filter.test(email))
+		{
+			alert('Invalid email');
+		}
+
+      else{
+         alert('you have registered successfully');
+      }
+
+
+ 
 }
-
-
